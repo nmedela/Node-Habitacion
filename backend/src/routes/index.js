cabedd44696c50d3f277e931d7f136cbecc04032
nmedela <medela.nicolas@gmail.com>
2019-08-01 21:50:19 -0300

    Comienzo con libreria de GPIO

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,10 +5,20 @@ const bodyParser = require('body-parser')
 
 const cors = require('cors')
 const lightRouter= require('./light')
+const soundRouter= require('./sound')
+const tvRouter= require('./tv')
+const curtainRouter= require('./curtain')
+const cameraRouter= require('./camera')
+
 
 appRouter.use(cors())
 appRouter.use(bodyParser.json())
 
 appRouter.use('/light', lightRouter)
+appRouter.use('/sound', soundRouter)
+appRouter.use('/tv', tvRouter)
+appRouter.use('/curtain', curtainRouter)
+appRouter.use('/camera', cameraRouter)
+
 
 module.exports = appRouter
