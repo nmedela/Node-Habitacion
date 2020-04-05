774ba48bc064fbb6e869717baf546f27fd6101f1
nmedela <medela.nicolas@gmail.com>
2020-04-05 16:54:11 -0300

    Cambio el import de moment

--- backend/src/deamon/daemonLight.js
+++ backend/src/deamon/daemonLight.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+const moment = require('moment')
 const { LightRepository } = require('./../repository/lightRepository')
 const { OptionRepository } = require('./../repository/optionRepositoryRepository')
 
