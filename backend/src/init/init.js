9155461ab592165772ff2fa029a8664d43e111e2
unknown <nicolas.medela@gmail.com>
2020-04-30 20:19:00 -0300

    cambio la forma de llamar al demon

--- backend/src/init/init.js
+++ backend/src/init/init.js
@@ -1,5 +1,5 @@
 const server = require('./initServer')
-const daemon = require('./../deamon/daemonLight')
+const daemon = require('./../deamon/daemonLight').init
 
 const { LightRepository } = require('./../repository/lightRepository')
 const init = async () => {
